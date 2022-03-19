5f50332615007ec64567bd3664c4ea5ee68144f8
Vittorio Gimmarino <vgiammar@scc1.bu.edu>
2022-03-19 19:28:37 -0400

    run results for RL simulations, not completed since GePPO requires rerun

---
+++ parallelize_offline_RL.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+for seed in $(seq 0 9);
+do
+qsub -l gpus=1 -l gpu_c=3.5 -l gpu_memory=16G SAC.qsub $seed 
+qsub -l gpus=1 -l gpu_c=3.5 -l gpu_memory=16G TD3.qsub $seed 
+qsub -l gpus=1 -l gpu_c=3.5 -l gpu_memory=16G PPO.qsub $seed 
+qsub -l gpus=1 -l gpu_c=3.5 -l gpu_memory=16G Vanilla_A2C.qsub $seed 
+qsub -l gpus=1 -l gpu_c=3.5 -l gpu_memory=16G A2C.qsub $seed 
+qsub -l gpus=1 -l gpu_c=3.5 -l gpu_memory=16G GePPO.qsub $seed 
+qsub -l gpus=1 -l gpu_c=3.5 -l gpu_memory=16G GeA2C.qsub $seed 
+
+done 
