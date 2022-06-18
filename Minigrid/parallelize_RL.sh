c1cf61d2c3a32afd00dfdc0cdc13daa692418f94
Vittorio Giammarino <vittoriogiammarino@pop-os.localdomain>
2022-06-18 17:14:00 -0400

    READme and final changes

--- Minigrid/parallelize_RL.sh
+++ Minigrid/parallelize_RL.sh
@@ -2,12 +2,12 @@
 
 for seed in $(seq 0 9);
 do
-#qsub -l gpus=1 -l gpu_c=3.5 -l gpu_memory=16G SAC.qsub $seed 
-#qsub -l gpus=1 -l gpu_c=3.5 -l gpu_memory=16G TD3.qsub $seed 
-qsub -l gpus=1 -l gpu_c=3.5 -l gpu_memory=16G PPO.qsub $seed 
-#qsub -l gpus=1 -l gpu_c=3.5 -l gpu_memory=16G Vanilla_A2C.qsub $seed 
-#qsub -l gpus=1 -l gpu_c=3.5 -l gpu_memory=16G A2C.qsub $seed 
-#qsub -l gpus=1 -l gpu_c=3.5 -l gpu_memory=16G GePPO.qsub $seed 
-#qsub -l gpus=1 -l gpu_c=3.5 -l gpu_memory=16G GeA2C.qsub $seed 
+#qsub -l gpus=1 -l gpu_c=3.5 -l gpu_memory=16 SAC.qsub $seed 
+#qsub -l gpus=1 -l gpu_c=3.5 -l gpu_memory=16 TD3.qsub $seed 
+qsub -l gpus=1 -l gpu_c=3.5 -l gpu_memory=16 QSUBS/RL/PPO.qsub $seed 
+#qsub -l gpus=1 -l gpu_c=3.5 -l gpu_memory=16 Vanilla_A2C.qsub $seed 
+#qsub -l gpus=1 -l gpu_c=3.5 -l gpu_memory=16 A2C.qsub $seed 
+#qsub -l gpus=1 -l gpu_c=3.5 -l gpu_memory=16 GePPO.qsub $seed 
+#qsub -l gpus=1 -l gpu_c=3.5 -l gpu_memory=16 GeA2C.qsub $seed 
 
 done 
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Leveraging-Videos-of-Rodent-Behavior-to-Accelerate-Reinforcement-Learning-from-Pixels-in-Navigation
+
+To install the anaconda environment follow the instruction here 
+https://docs.conda.io/projects/conda/en/latest/user-guide/tasks/manage-environments.html#creating-an-environment-from-an-environment-yml-file
+the environment.yml file contains more packages than those actually needed. 
+
+## TORUN
+
+We care only about the Minigrid folder: if you run on the SCC the bash files parallelize_RL.sh and parallelize_on_off_RL_from_observations_rodent_ri_***.sh will send 10 batch submission to the cluster
+each batch file is associated to a QSUB file which is in the QSUB folder.
+
+Watch out that the .qsub file calls the miniconda environment, you should change the line with the folder in which you save the environment on the SCC 
