c28df64c0aec1de351994b8604f590ccc51ad220
Vittorio Giammarino <vittoriogiammarino@pop-os.localdomain>
2022-06-18 16:53:08 -0400

    environment for minigrid all set + bash files

---
+++ Minigrid/parallelize_on_off_RL_from_observations_rodent_ri_0.005.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+for seed in $(seq 0 2);
+do
+#qsub -l gpus=1 -l gpu_c=3.5 -l gpu_memory=16 QSUBS/on_off_RL_observations/rodent_domain_adaptation_ri_0.005/AWAC_GAE.qsub $seed 
+#qsub -l gpus=1 -l gpu_c=3.5 -l gpu_memory=16 QSUBS/on_off_RL_observations/rodent_domain_adaptation_ri_0.005/AWAC_Q_lambda_Haru.qsub $seed 
+#qsub -l gpus=1 -l gpu_c=3.5 -l gpu_memory=16 QSUBS/on_off_RL_observations/rodent_domain_adaptation_ri_0.005/AWAC_Q_lambda_Peng.qsub $seed 
+#qsub -l gpus=1 -l gpu_c=3.5 -l gpu_memory=16 QSUBS/on_off_RL_observations/rodent_domain_adaptation_ri_0.005/AWAC_Q_lambda_TB.qsub $seed 
+qsub -l gpus=1 -l gpu_c=3.5 -l gpu_memory=16 QSUBS/on_off_RL_observations/rodent_domain_adaptation_ri_0.005/SAC.qsub $seed 
+qsub -l gpus=1 -l gpu_c=3.5 -l gpu_memory=16 QSUBS/on_off_RL_observations/rodent_domain_adaptation_ri_0.005/AWAC.qsub $seed 
+
+done 
